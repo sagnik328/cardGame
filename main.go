7a9ecda97b37ef8d6d8a5da0e8fc035f074c8027
Sagnik Chatterjee <sagnikchatterjee328@gmail.com>
2022-10-20 11:31:51 +0530

    src: showing ways to define variables in go

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of spades"
+	//	var card string = "Ace of spades"
+	card := "Ace of spades"
+	card = "New value for ace of spades"
 	fmt.Println(card)
 }
